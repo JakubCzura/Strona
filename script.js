9158ab318e5a89422b62f9d0191844b42414a3cf
Jakub Czura <jakubczura@wp.pl>
2021-05-18 15:12:02 +0200

    zegarek

---
+++ script.js
@@ -0,0 +1,37 @@
+function getTime()
+    {
+    var data = new Date();
+
+    var dni=data.getDate();
+    if(dni<10)
+    {
+        dni = "0"+dni;
+    }
+    var miesiace=data.getMonth() + 1; /* Bo numeruje od 0 do 11 */
+    if (miesiace<10)
+    {
+        miesiace="0"+miesiace;
+    }
+    var lata=data.getFullYear(); 
+
+    var godziny=data.getHours();
+    if(godziny<10)
+    {
+        godziny = "0"+godziny;
+    }
+    var minuty=data.getMinutes();
+    if(minuty<10)
+    {
+        minuty = "0"+minuty;
+    }
+    var sekundy=data.getSeconds();
+    if(sekundy<10)
+    {
+        sekundy = "0"+sekundy;
+    }
+
+    document.getElementById("currentTime").innerHTML = "Dzisiaj: "+dni+":"+miesiace+":"+lata+ "<br/>" + 
+    "Godzina: "+godziny+":"+minuty+":"+sekundy;
+
+    setTimeout("getTime()",1000); /* wywoływanie funkcji co 1000 milisekund */
+}
