3deadf5170ee4d517eb07b606165e30398353b99
Jakub Czura <jakubczura@wp.pl>
2021-05-24 00:23:25 +0200

    działa wysłanie do bazy danych formularza opini

--- res.php
+++ res.php
@@ -15,28 +15,147 @@
 
 <body class="body" onload="getTime()">
 
-<?php
+<div class="jumbotron-text-center">
+  <h1>Witaj na stronie fanów Iron Maiden</h1>
+  <p>Zapraszamy do lektury i posłuchania ich piosenek</p> 
+</div>
+  
+<div class="container">
+    <div id="currentTime"></div>
+    
+    <div class="youtubeLink">
+    <?php
     include_once 'database.php';
     $name = $_GET['f_name'];
     $lastName = $_GET['l_name'];
     $message = $_GET['message'];
 
-    echo $_GET['f_name'];
-    echo $_GET['l_name'];
-    echo $_GET['message'];
-
     $sql = "INSERT INTO Comments (Name, LastName, Comment) VALUES ('$name', '$lastName', '$message')";
 
     if(mysqli_query($conn, $sql))
     {
-        echo "Udane zapytanie<br>";
+        echo "Dziękujemy za dodanie opinii<br>";
     }
     else
     {
         echo "Błąd ".mysqli_error($conn). "<br>";
     }
     mysqli_close($conn);
-?>
+    
+    echo "Imię: ".$_GET['f_name']."<br>";
+    echo "Nazwisko: ".$_GET['l_name']."<br>";
+    echo "Opinia o albumie: ".$_GET['message']."<br>";
+    ?>
+  </div> 
+
+  
+  <div class="row"> 
+      <div id="navbar">
+      <a class="active" href="index.php">Strona główna</a>
+      <a href="news.php">Aktualności</a>      
+      <a href="albums.php">Albumy</a> 
+      <a href="tours.php">Trasa</a> 
+      <a href="photo.php">Galeria zdjęć</a> 
+      <a href="https://www.facebook.com/">Kontakt</a> 
+      </div>
+    <div class="col-sm-4 photo">
+      <h3>Iron Maiden</h3>
+      <p>Pierwszy album Iron Maiden. Zaprezentowali na nim niespotykane dotąd ludzkości
+      brzmienia i zupełnie nowy pomysł na tworzenie muzyki.</p>        
+      <a href="iron.php"> <img src="graphics/1.jpg" alt="iron maiden album" width="200"> </a>
+    </div>
+    <div class="col-sm-4 photo">
+      <h3>Killers</h3>  
+      <p>Drugi album grupy. Rozwinęli na nim swoje umiejętności, prezentując kawałki
+      w stylu klasycznego metalu. Charakteryzstyczny Eddie znów zaskakuje na okładce.</p>        
+      <a href="killers.php"> <img src="graphics/2.jpg" alt="iron maiden album" width="200"> </a> 
+    </div>
+    <div class="col-sm-4 photo">
+      <h3>The Number of the beast</h3> 
+      <p>Trzeci album zespołu. Pochodzą z niego jedne z największych hitów jak Hallowed
+      By Thy Name czy Run to the Hills.</p> 
+      <a href="number.php"> <img src="graphics/3.jpg" alt="iron maiden album" width="200"> </a>
+    </div>
+    <div class="col-sm-4 photo">
+      <h3>Piece of Mind</h3> 
+      <p>Czwarty album Iron Maiden. Jedno z największych dokonań grupy, słynny The Trooper,
+      genialna okłada i perfekcyjne brzmienie gitar to znak rozpoznawczy.</p>         
+      <a href="mind.php"> <img src="graphics/4.jpg" alt="iron maiden album" width="200"> </a>
+    </div>
+    <div class="col-sm-4 photo">
+      <h3>Powerslave</h3>
+      <p>Piąty album zespołu. Znany z melodyjnych piosenek, bardzo dobrych partii wokalnych,
+      Aces High z płyty często rozpoczyna koncerty.</p>          
+      <a href="powerslave.php"> <img src="graphics/5.jpg" alt="iron maiden album" width="200"> </a>
+    </div>
+    <div class="col-sm-4 photo">
+      <h3>Somewhere in Time</h3>   
+      <p>Szósty album zespołu. Zaprezentowali na nim rytmiczne partie gitarowe. Teledysk utworu 
+      Wasted Years zawiera wzmianki z koncertów z Polski.</p>       
+      <a href="time.php"> <img src="graphics/6.jpg" alt="iron maiden album" width="200"> </a> 
+    </div>
+    <div class="col-sm-4 photo">
+      <h3>Seventh Son of a Seventh Son</h3>   
+      <p>Siódmy album zespołu. Zawiera długie kompozycje o odmiennym dotąd klimacie. Ballady 
+      są bardzo wzruszające.</p>       
+        <a href="son.php"> <img src="graphics/7.jpg" alt="iron maiden album" width="200"> </a> 
+    </div>
+    <div class="col-sm-4 photo">
+      <h3>No Prayer for the Dying</h3>   
+      <p>Ósmy album zespołu. Zadebiutował na nim Janick Gers, gitarzysta o polskich korzeniach. 
+      Świetne i chwytliwe riffy przyciągają uszy słuchaczy.</p>       
+      <a href="prayer.php"> <img src="graphics/8.jpg" alt="iron maiden album" width="200"> </a> 
+    </div>
+    <div class="col-sm-4 photo">
+      <h3>Fear of the Dark</h3>   
+      <p>Dziewiąty album grupy. Tytułowy utwór tworzy niesamowitą atmosferę na koncertach 
+      i jest śpiewany przez publiczność.</p>       
+      <a href="fear.php"> <img src="graphics/9.jpg" alt="iron maiden album" width="200"> </a>
+    </div> 
+    <div class="col-sm-4 photo">
+      <h3>The X Factor</h3>   
+      <p>Dziesiąty album Iron Maiden. Zawiera długie i bardzo nostalgiczne kompozycje. 
+      Wymaga skupienia, by go zrozumieć.</p>       
+      <a href="factor.php"> <img src="graphics/10.jpg" alt="iron maiden album" width="200"> </a> 
+    </div> 
+    <div class="col-sm-4 photo">
+      <h3>Virtual XI</h3>   
+      <p>Jedenasty album zespołu. Nagrany z Blazem Bayleyem nie był tak udany jak poprzednie,
+      ale fani wciąż doceniają bardzo duży wkład sił w jego powstanie.</p>       
+      <a href="virtual.php"> <img src="graphics/11.jpg" alt="iron maiden album" width="200"> </a> 
+    </div> 
+    <div class="col-sm-4 photo">
+      <h3>Brave new World</h3>   
+      <p>Dwunasty album zespoły. Przełomowe nagranie, na którym występuje klasyczny skład Iron Maiden,
+      bardzo udane i chwytliwe melodie to jego mocna cecha.</p>       
+      <a href="world.php"> <img src="graphics/12.jpg" alt="iron maiden album" width="200"> </a> 
+    </div> 
+    <div class="col-sm-4 photo">
+      <h3>Dance of Death</h3>   
+      <p>Trzynasty album grupy. Porusza motyw śmierci w muzyczny sposób. 
+      Lekkie kompozycje zostały wzmocnione bardzo silnymi brzmieniami gitar.</p>       
+      <a href="dance.php"> <img src="graphics/13.jpg" alt="iron maiden album" width="200"> </a>
+    </div> 
+    <div class="col-sm-4 photo">
+      <h3>A Matter of Life and Death</h3>   
+      <p>Czternasty album zespołu. Bardzo klasyczne brzmienia oparte na sztuce dobrego rocka. 
+      Album jest spójną kompozycją, a piosenki nawzajem się uzupełniają.</p>       
+      <a href="matter.php"> <img src="graphics/14.jpg" alt="iron maiden album" width="200"> </a>
+    </div> 
+    <div class="col-sm-4 photo">
+      <h3>The Final Frontier</h3>   
+      <p>Piętnasty album grupy. Bardzo ciekawa okłada oraz wspaniałe kompozycje, które dodały
+      klimatu podczas trasy koncertowej promującej album.</p>       
+      <a href="frontier.php"> <img src="graphics/15.jpg" alt="iron maiden album" width="200"> </a>
+    </div> 
+    <div class="col-sm-4 photo">
+      <h3>The Book of Souls</h3>   
+      <p>Szesnasty album grupy. Najnowsze dzieło to porcja brzmień w najlepszym wydaniu,
+      kompozycje z lekkim wstępem przechodzą w doskonałe metalowe brzmienie.</p>       
+      <a href="book.php"> <img src="graphics/16.jpg" alt="iron maiden album" width="200"> </a>
+    </div>
+  </div>
+</div>
 
 <div class="footer">
   <footer id="foot">
@@ -44,6 +163,8 @@
     <p>Życzymy miłego dnia</p>
   </footer> 
 </div>
+
+
 
 <script>
 window.onscroll = function() {myFunction()};
